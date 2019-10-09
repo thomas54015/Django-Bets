3dbb7f7c0c06e53e130aa46fecbb7d00a167da9e
Thomas Rudolph <shadowfox@localhost.localdomain>
2019-10-09 17:37:56 -0400

    File added
    I added this file so that for php that will use in most php files. 
    currently only contain db access.

---
+++ functions.php
@@ -0,0 +1,14 @@
+<?php
+// I put this here incase we need to change the database information.
+// This way we would only have to change it here instead of everywhere.
+$servername = "localhost";
+$username = "djangoadmin";
+$password = "bestgroup";
+$dbname = "localhost";
+
+/*
+MySQL command to add a new user to database. Not needed for anyone who doesn't
+want to use a database locally. **Grants user full rights to everything**
+GRANT ALL PRIVILEGES ON *.* TO 'djangoadmin'@'localhost' IDENTIFIED BY 'bestgroup';
+*/
+?>
