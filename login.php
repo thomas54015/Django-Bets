93a82e2eaf8c6d32973ec2bc887028aae43f8eb5
Thomas Rudolph <shadowfox@localhost.localdomain>
2019-10-09 18:58:13 -0400

    php html structure
    I think I am finished with the layout design of the login for now. I 
    will go back and add the php to it later.

---
+++ login.php
@@ -0,0 +1,39 @@
+<html>
+<head>
+  <title>Login</title>
+  <link rel="stylesheet" href="./css/login.css">
+  <link rel="icon" href="./pics/danjosfantasy.jpg">
+</head>
+<body>
+  <div class="banner">
+    <img class="page-logo" src="./pics/logo.png" alt="logo">
+  </div>
+  <div class="main">
+    <div class="loginWrap">
+      <div class="cTitle">
+      Login to Django
+    </div>
+    <div class="inputWrap">
+      <div class="inputLeft">
+        Username:
+      </div>
+      <div class="inputRight">
+        <input type="text" />
+      </div>
+    </div>
+    <div class="inputWrap">
+      <div class="inputLeft">
+        Password:
+      </div>
+      <div class="inputRight">
+        <input type="password" />
+      </div>
+    </div>
+    <div class="inputWrap">
+      <div class="inputCenter">
+        <input class="formButton" type="submit" value="Login" />
+      </div>
+    </div>
+    </div>
+  </div>
+</body>
